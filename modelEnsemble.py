4a6b762d209137c69d503359dd58b4d9eccce51a
sabrinanegroni <negroni.sabrina@gmail.com>
2023-11-14 15:47:00 +0100

    ensamble

--- modelEnsemble.py
+++ modelEnsemble.py
@@ -17,11 +17,20 @@ class modelEnsemble:
             predictions = np.round(m.predict(X))
             labels.append(predictions)
 
-        
+        # Debugging: Print shapes of predictions
+        for predictions in labels:
+            print(predictions.shape)
+
         labels = np.array(labels)
         labels = np.transpose(labels, (1, 0))
-        ensemble_labels = stats.mode(labels, axis=-1)[0]
+
+        # Debugging: Print shape of labels before mode
+        print(labels.shape)
+
+        ensemble_labels = mode(labels, axis=-1)[0]
         ensemble_labels = np.squeeze(ensemble_labels)
 
+        # Debugging: Print shape of ensemble_labels before returning
+        print(ensemble_labels.shape)
 
         return ensemble_labels
