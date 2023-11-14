4f7b99a56b1146f10cfd927187cc11d214c9aed2
sabrinanegroni <negroni.sabrina@gmail.com>
2023-11-14 11:30:17 +0100

    esemble

---
+++ submission/modelEnsemble.py
@@ -0,0 +1,28 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+
+class modelEnsamble:
+    def __init__(self, model_paths):
+        self.models = [tf.keras.models.load_model(model_path) for model_path in model_paths]
+
+
+    def predict(self, X):
+        
+        labels=[]
+        
+        for m in self.models:
+            predictions = np.round(m.predict(X,axis=1))
+            labels.append(predicts)
+
+        
+        labels = np.array(labels)
+        labels = np.transpose(labels, (1, 0))
+        ensemble_labels = mode(labels, axis=-1)[0]
+        ensemble_labels = np.squeeze(ensemble_labels)
+
+        predictions = predictions.astype(int)
+        predictions = tf.convert_to_tensor(predictions)
+
+        return predictions
